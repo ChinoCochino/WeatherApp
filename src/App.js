58d156a59c9d921b51d5f8104ddf1aaf39215478
Oroko <aoroko09@gmail.com>
2020-07-02 16:05:46 -0400

    Testing CitiesOutput component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
+import CitiesOutput from "./Components/CitiesOutput";
 
 import "./App.css";
 
 class App extends Component {
   render() {
-    return <div className="App">test</div>;
+    return (
+      <div className="App">
+        test
+        <CitiesOutput />
+      </div>
+    );
   }
 }
 
